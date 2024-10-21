ac5e9ca64a0c817c49ad89df2785bb0e9ddf6a28
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-10-21 18:22:08 -0500

    4.1

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -34,6 +34,7 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setPathMatcher(new AntPathMatcher(".")); //Point convention
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");        
     }
